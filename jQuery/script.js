1a2f43b4256a6d5ec94150f6daabf4c5b65913bb
Kim Duclos <kimduclos@gmail.com>
2018-04-08 17:09:33 -0600

    Final Update

--- jQuery/script.js
+++ jQuery/script.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     $("#target4").remove();
     $("#target2").appendTo("#right-well");
     $("#target3").appendTo("left-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $(".target:nth-child(2)").addClass("animated bounce")
+    $(".target:even").addClass("animated shake");
+    $("body").addClass("animated hinge");
+
 });
