38cd437914ced95dc3c143a7e3ad3cc5dd50453c
Kim Duclos <kimduclos@gmail.com>
2018-04-11 08:22:37 -0600

    Update

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,38 @@
-// This is an in-line comment
 
-/* This is a
-multi-line comment */
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+  }
+  
+  var betHold;
+  
+  if (count > 0) {
+    betHold = " Bet";
+  }
+  else if (count <= 0) {
+   betHold = " Hold"; 
+  }
+  
+  return count + betHold;
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(4); cc(5); cc(6);
